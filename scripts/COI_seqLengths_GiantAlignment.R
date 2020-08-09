5e22f93e3ece16a21eca3c756bc17dc28ca42a5b
Devon O'Rourke <devon.orourke@gmail.com>
2020-08-09 06:46:18 -0400

    Update COI_seqLengths_GiantAlignment.R

--- scripts/COI_seqLengths_GiantAlignment.R
+++ scripts/COI_seqLengths_GiantAlignment.R
@@ -9,18 +9,23 @@ library(scales)
 ## load data and plot
 ################################################################################
 
-df <- read_table("https://raw.githubusercontent.com/devonorourke/COIdatabases/master/data/anml_seq_lengths.freq.table", col_names=FALSE)
+df <- read_delim(file="https://raw.githubusercontent.com/devonorourke/COIdatabases/master/data/anml_seq_lengths.freq.table", 
+                 col_names=FALSE, delim=" ")
 colnames(df) <- c("NumberOfSeqs", "SeqLength")
+df$SeqLength <- as.integer(df$SeqLength)
+df$NumberOfSeqs <- as.integer(df$NumberOfSeqs)
 sumSeqs <- sum(df$NumberOfSeqs)
 df <- df %>%
   mutate(SeqLengtf = as.numeric(SeqLength)) %>%
   arrange(SeqLength) %>%
   mutate(CumSum = cumsum(NumberOfSeqs)) %>%
   mutate(fracSeqs = CumSum/sumSeqs)
+
 ggplot(df, aes(x=SeqLength, y=NumberOfSeqs)) +
   geom_bar(stat="identity") +
   theme_bw() +
   scale_y_continuous(labels=comma) +
   labs(x="\nSequence length (bp)", y="Number of sequences\n")
 
-ggsave("anml_primer_COI_seqLength.png", height=10, width=15, units="cm") 
+setwd('/Users/devonorourke/github/COIdatabases/figures')
+ggsave("anml_primer_COI_seqLength.png", height=10, width=15, units="cm")
